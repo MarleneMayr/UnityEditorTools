5c80c6cee91808e4e5195e53d584ad7516741da9
mayrmarlene <marlene.mayr@innerspace.at>
2019-04-25 10:17:31 +0200

    added quickOverview tool to implement scene/prefab handling

---
+++ Assets/Editor/QuickOverview/QuickOverview.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace EditorTools
+{
+    class QuickOverview : EditorWindow
+    {
+        public static void Init()
+        {
+            // TODO get relevant elements
+        }
+    }
+}
